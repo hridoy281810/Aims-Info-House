8997205abaa9da706e5cfad86f879ac3e0a03df6
hridoy281810 <hridoy01724281810@gmail.com>
2023-03-31 00:09:52 +0600

    Spent time on read handaler added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -9,7 +9,16 @@ useEffect(()=>{
  
 },[readTime]);
     return (
-        <div>
+        <div className='cart'>
+            <div className="spent-time">
+                <p>Spent time on read : 177 min</p>
+            </div>
+            <div className="add-cart">
+                <h3>Bookmarked Blogs :</h3>
+                <div className="add-cart-title">
+                <p>Master Microsoft Power Platform and Become an In-Demand!</p>
+                </div>
+            </div>
             <h1>ashd {readTime} </h1>
         </div>
     );
